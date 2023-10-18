83dac9f815a8fc2398c1d8e06817391e2f806922
Aromal7800 <249228@ust.com>
2023-10-18 17:37:03 +0530

    Assignment-1-oct-18

---
+++ Assignment-1-oct-18/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Assignment_1_oct_18;
+Console.WriteLine("Enter the student Name");
+string name=Console.ReadLine();
+Console.WriteLine("Enter the student marks");
+int markOne=Convert.ToInt32(Console.ReadLine());
+int markTwo = Convert.ToInt32(Console.ReadLine());
+int markThree = Convert.ToInt32(Console.ReadLine());
+
+Student student = new Student(name,markOne,markTwo,markThree);
+Console.WriteLine("Student Name :"+name);
+Console.WriteLine("Total Marks :" + student.TotalMarks());
+Console.WriteLine("Average Marks :"+student.Average());
+
+Console.WriteLine("Grade :"+student.Grade());
