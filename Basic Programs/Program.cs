bf63e165bccebfa40b35b5af3061250c7c7bb6f9
Aromal7800 <249228@ust.com>
2023-10-18 16:59:53 +0530

    Add project files.

---
+++ Basic Programs/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Basic_Programs;
+Electricity electricity1=new(12345,9000,9300,"XX");
+double billamount = electricity1.CalculateBill();
+Console.WriteLine(electricity1.CalculateBill());
+Console.WriteLine(electricity1.consumernumber);
+Console.WriteLine(electricity1.consumername);
+Electricity electricity2 = new(12345, 7000, 9300, "XX");
+
+Console.WriteLine(electricity2.CalculateBill());
+/*
+ * 
+Console.WriteLine("Enter two numbers");
+int num1, num2, ans;
+num1=Convert.ToInt32(Console.ReadLine());
+num2=Convert.ToInt32(Console.ReadLine());
+ans = num1 + num2;
+Console.WriteLine(ans);
+string? s1="";
+String? s2="";
+string? s3="";
+s3 = s1 +"  "+s2;
+Console.WriteLine(s3);  
+int x;
+s1=Console.ReadLine();
+s2=Console.ReadLine();
+Console.WriteLine(s1.Equals(s2));
+Console.WriteLine(s1.ToLower());
+Console.WriteLine(s2.ToUpper());
+s2.Trim();
+Console.WriteLine(s3.Trim());
+
+string[] s=s3.Split(' ');
+
+for(int i = 0; i < s.Length; i++)
+{
+    Console.WriteLine(s[i]);
+}
+*/
+calulation calculation=new calulation();
+int num1=Convert.ToInt32(Console.ReadLine());
+int num2=Convert.ToInt32(Console.ReadLine());
+double ans=calculation.add(num1, num2);
+Console.WriteLine(ans);
---
+++ Basic Programs/calulation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Programs
+{
+    internal class calulation
+    {
+        double num1, num2, ans;
+        public double add(int num1,int num2) { ans=num1 + num2;
+            return(ans);
+
+        }
+
+    }
+}
